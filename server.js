c5750e938553254eede5b3e7bd8ff0c1343c8a52
JCH95 <jhorger95@gmail.com>
2022-03-28 20:20:52 -0500

    added more requires and the port for the server

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const uniqueid = require('uniqid');
+
+
+
+// Middleware
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Parse incoming JSON data
+app.use(express.json());
+// Parse incoming css/js data
+app.use(express.static('public'));
+
+
+
+// Starts the server and console logs if it's working
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
