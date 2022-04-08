4b62c0d1a9705d9159c33f0b3a32233aa37975dd
JCH95 <jhorger95@gmail.com>
2022-04-07 20:09:54 -0500

    cant remember what i changed here but i need to commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,41 +5,7 @@ const { User, Organization } = require('../models'); // schema for the data and 
 // get all users for homepage
 router.get('/', (req, res) => {
     console.log('======================');
-    User.findAll({ // sequelize syntax. Pulls in post from models
-        attributes: [
-            'id',
-            'username',
-            'email',
-            'wins',
-            'losses',
-            'elo',
-            // update what we need from db
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'] // 
-        ],
-        // include: [
-        //     {
-        //         model: Comment,
-        //         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        //         include: {
-        //             model: User,
-        //             attributes: ['username']
-        //         }
-        //     },
-        //     {
-        //         model: User,
-        //         attributes: ['username']
-        //     }
-        // ]
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-
-            res.render('homepage', { posts });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    res.render('homepage');
 });
 
 
